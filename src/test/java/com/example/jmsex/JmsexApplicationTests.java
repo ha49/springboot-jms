399d7b1d3a246d5d4cfc07c41cdcfd7917d3f6dc
ha49 <kaysersoza@outlook.com>
2021-01-14 03:41:22 +0100

    initial commit

---
+++ src/test/java/com/example/jmsex/JmsexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
