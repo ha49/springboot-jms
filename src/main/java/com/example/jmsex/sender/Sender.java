e1511299c1362af06b51dc7f0281aec409c9e7de
ha49 <kaysersoza@outlook.com>
2021-01-14 03:43:31 +0100

    added jms template

--- src/main/java/com/example/jmsex/sender/Sender.java
+++ src/main/java/com/example/jmsex/sender/Sender.java
@@ -1,6 +1,8 @@
 package com.example.jmsex.sender;
 
 import com.example.jmsex.model.HelloMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +12,20 @@ public class Sender {
     private int counter=1;
 
 
+    @Autowired
+    JmsTemplate jmsTemplate;
+
 
 
     @Scheduled(fixedRate = 3000)
     public void sendMessage(){
 
-        System.out.println("sending message - " + counter++);
+        System.out.println("sending message - " + counter);
         HelloMessage helloMessage=new HelloMessage("hello");
+
+        jmsTemplate.convertAndSend("our-queue", helloMessage );
+
+        System.out.println("message - " + counter+ " sent");
 
 
 
