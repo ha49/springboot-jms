03f067ad0cfc83f3f264e8553ecaa42b2ebc7c33
ha49 <kaysersoza@outlook.com>
2021-01-18 16:29:03 +0100

    added jms receiver

--- src/main/java/com/example/jmsex/sender/Sender.java
+++ src/main/java/com/example/jmsex/sender/Sender.java
@@ -1,14 +1,8 @@
 package com.example.jmsex.sender;
 
-import com.example.jmsex.config.JmsConfig;
-import com.example.jmsex.model.MessageObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Component
 public class Sender {
@@ -22,23 +16,20 @@ public class Sender {
 
 
 
-    @Scheduled(fixedRate = 3000)
-    public void sendMessage(){
-
-        System.out.println("sending message " + counter);
-        MessageObject messageObject =new MessageObject(
-                UUID.randomUUID(),
-                "Hello from FLX_QUEUE ..",
-                LocalDateTime.now());
-
-        jmsTemplate.convertAndSend(JmsConfig.FLX_QUEUE, messageObject);
-
-        System.out.println("message - " + counter+ " sent");
-//        System.out.println(messageObject);
-
-        counter++;
-
-
-
-    }
+//    @Scheduled(fixedRate = 3000)
+//    public void sendMessage(){
+//
+//        System.out.println("sending message " + counter);
+//        MessageObject messageObject =new MessageObject(
+//                UUID.randomUUID(),
+//                "Hello from FLX_QUEUE ..",
+//                LocalDateTime.now());
+//
+//        jmsTemplate.convertAndSend(JmsConfig.FLX_QUEUE, messageObject);
+//
+//        System.out.println("message - " + counter+ " sent");
+////        System.out.println(messageObject);
+//
+//        counter++;
+//    }
 }
