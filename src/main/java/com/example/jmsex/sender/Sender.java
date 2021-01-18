4d6e0373abcb38c8b1c06acc3eede2b0ef16fe25
ha49 <kaysersoza@outlook.com>
2021-01-18 18:35:31 +0100

    added embedded artemis server dependencies

--- src/main/java/com/example/jmsex/sender/Sender.java
+++ src/main/java/com/example/jmsex/sender/Sender.java
@@ -1,8 +1,14 @@
 package com.example.jmsex.sender;
 
+import com.example.jmsex.config.JmsConfig;
+import com.example.jmsex.model.MessageObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.util.UUID;
+
+import java.time.LocalDateTime;
 
 @Component
 public class Sender {
@@ -16,20 +22,20 @@ public class Sender {
 
 
 
-//    @Scheduled(fixedRate = 3000)
-//    public void sendMessage(){
-//
-//        System.out.println("sending message " + counter);
-//        MessageObject messageObject =new MessageObject(
-//                UUID.randomUUID(),
-//                "Hello from FLX_QUEUE ..",
-//                LocalDateTime.now());
-//
-//        jmsTemplate.convertAndSend(JmsConfig.FLX_QUEUE, messageObject);
-//
-//        System.out.println("message - " + counter+ " sent");
-////        System.out.println(messageObject);
-//
-//        counter++;
-//    }
+    @Scheduled(fixedRate = 3000)
+    public void sendMessage(){
+
+        System.out.println("sending message " + counter);
+        MessageObject messageObject =new MessageObject(
+                UUID.randomUUID(),
+                "Hello from FLX_QUEUE ..",
+                LocalDateTime.now());
+
+        jmsTemplate.convertAndSend(JmsConfig.FLX_QUEUE, messageObject);
+
+        System.out.println("message - " + counter+ " sent");
+//        System.out.println(messageObject);
+
+        counter++;
+    }
 }
